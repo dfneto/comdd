0586289990f63593c679baaa4a7dc5f66fda3fac
davidfneto <davidfneto@fb8313af-4c01-193a-e81d-37fb70c46c91>
2011-01-10 19:58:27 +0000

    Nesta versao da pasta jar, temos: uma estrutura de pacotes e uma dsl com essa estrutura. Ela foi criada a partir das seguintes classes: david@linux-touch:~/comdd/ANTLR_Codigos/jar$ jar -cvfm dsl.jar manifest.txt antlr-3.0 classes source Todas as classes e fontes da dsl normal (sem ser em jar), mais todo o antlr Como saída tive: david@linux-touch:~/comdd/ANTLR_Codigos/jar$ java -jar dsl.jar Exception in thread "main" java.lang.NoClassDefFoundError: Test Caused by: java.lang.ClassNotFoundException: Test 	at java.net.URLClassLoader$1.run(URLClassLoader.java:202) 	at java.security.AccessController.doPrivileged(Native Method) 	at java.net.URLClassLoader.findClass(URLClassLoader.java:190) 	at java.lang.ClassLoader.loadClass(ClassLoader.java:307) 	at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:301) 	at java.lang.ClassLoader.loadClass(ClassLoader.java:248) Could not find the main class: Test. Program will exit.

---
+++ ANTLR_Codigos/jar/antlr-3.0/runtime/ObjC/Framework/examples/combined/CombinedParser.h
@@ -0,0 +1,34 @@
+// $ANTLR 3.0b6 Combined.g 2007-02-01 01:27:56
+
+#import <Cocoa/Cocoa.h>
+#import <ANTLR/ANTLR.h>
+
+
+#pragma mark Cyclic DFA
+
+#pragma mark Tokens
+#define CombinedParser_INT	5
+#define CombinedParser_WS	6
+#define CombinedParser_EOF	-1
+#define CombinedParser_ID	4
+
+#pragma mark Dynamic Global Scopes
+
+#pragma mark Dynamic Rule Scopes
+
+#pragma mark Rule Return Scopes
+
+
+@interface CombinedParser : ANTLRParser {
+
+			
+
+ }
+
+
+- (void) stat;
+- (void) identifier;
+
+
+
+@end
