c4e279ecbc83b2203065cea15cb3c532e4c9309b
davidfneto@gmail.com <davidfneto@gmail.com@fb8313af-4c01-193a-e81d-37fb70c46c91>
2011-01-18 03:02:21 +0000

    LeituraBancoEscritaTxt.java está, por hora, só lendo do banco. Próximo passo é gerar num txt a saída do terminal

--- trunk/ProgramaJDBC/Code/LeituraBancoEscritaTxt.java
+++ trunk/ProgramaJDBC/Code/LeituraBancoEscritaTxt.java
@@ -1,19 +1,14 @@
-//Lendo do Banco:
+//Leitura do banco e escrita num txt
 import java.sql.*;
 
-public class LendoBanco {
+public class LeituraBancoEscritaTxt {
 	public static void main(String[] argv) {
 		//declarando as variáveis:	 
 		Connection connection = null;
 		String leitura = "select xwd_content from xwikidoc where xwd_name='TesteNome'"; 
-	//	String escrita = "UPDATE xwikidoc SET xwd_content='Chocolate Feliz' where xwd_name='TesteNome'";
-		String CodigoGeradoPelaDsl = "Aeeeeeeeeeeeeeee";
-		String escrita = "UPDATE xwikidoc SET xwd_content='" + CodigoGeradoPelaDsl + "' where xwd_name='TesteNome'";
 		Statement stmt; 		
 		String url = "jdbc:postgresql://localhost:5432/xwiki";
 
-		System.out.println("-------- PostgreSQL JDBC Connection Testing ------------");
- 
 		try {
  			Class.forName("org.postgresql.Driver"); //Carrega o driver do postgresql
 		} catch (ClassNotFoundException e) {
@@ -21,24 +16,6 @@ public class LendoBanco {
 			e.printStackTrace();
 			return;	}
  		
-		System.out.println("PostgreSQL JDBC Driver Registered! \n Valendo !!");
- 
-		//Connection connection = null;
- 
-		try { 
-			connection = DriverManager.getConnection(
-				"jdbc:postgresql://127.0.0.1:5432/xwiki","xwiki", "xwiki"); //conectando ao banco
- 		} catch (SQLException e) {
- 			System.out.println("Connection Failed! Check output console");
-			e.printStackTrace();
-			return;	}
- 
-		if (connection != null){
-			System.out.println("You made it, take control your database now! -- Valendo 2!!!");
-		}else{
-			System.out.println("Failed to make connection!");
-		}
-
 		try { 
 			connection = DriverManager.getConnection(url,"postgres", "12345"); 			
 			//Leitura (SELECT)
@@ -48,6 +25,7 @@ public class LendoBanco {
 			ResultSetMetaData rsmd = rs.getMetaData(); 
 			int numberOfColumns = rsmd.getColumnCount(); 
 			int rowCount = 1; 
+			
 			//Imprimindo os resultados no terminal		
 			System.out.println("Modelo (entrada): \n"); 
 			while (rs.next()) { 
@@ -61,10 +39,7 @@ public class LendoBanco {
 		       System.out.println(""); 
 		       rowCount++; 
 		       }
-			//Alterando o valor do campo (UPDATE)
-			stmt.executeUpdate(escrita);
-			System.out.println("O valor foi alterado: \n String usada: "+escrita+ "\n Valor Alterado para: \n" + CodigoGeradoPelaDsl);	 
-
+			
 		   	stmt.close(); 
 		   	connection.close(); 
 
