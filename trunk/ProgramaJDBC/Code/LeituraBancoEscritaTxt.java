d8ec806c3b6193026021124375fd9eb4e32f4f3c
davidfneto@gmail.com <davidfneto@gmail.com@fb8313af-4c01-193a-e81d-37fb70c46c91>
2011-01-18 03:33:09 +0000

    Agora LeituraBancoEscritaTxt está gerando um txt com um texto qqr. Falta agora ele gerar com o texto lido do banco

--- trunk/ProgramaJDBC/Code/LeituraBancoEscritaTxt.java
+++ trunk/ProgramaJDBC/Code/LeituraBancoEscritaTxt.java
@@ -1,21 +1,24 @@
 //Leitura do banco e escrita num txt
 import java.sql.*;
+import java.io.*;
 
-public class LeituraBancoEscritaTxt {
+public class LeituraBancoEscritaTxt {//Leitura e escrita diretamente no Banco:
 	public static void main(String[] argv) {
 		//declarando as variáveis:	 
 		Connection connection = null;
 		String leitura = "select xwd_content from xwikidoc where xwd_name='TesteNome'"; 
+	
 		Statement stmt; 		
 		String url = "jdbc:postgresql://localhost:5432/xwiki";
 
+ 
 		try {
  			Class.forName("org.postgresql.Driver"); //Carrega o driver do postgresql
 		} catch (ClassNotFoundException e) {
  			System.out.println("Where is your PostgreSQL JDBC Driver? \n Include in your library path!");
 			e.printStackTrace();
 			return;	}
- 		
+ 			
 		try { 
 			connection = DriverManager.getConnection(url,"postgres", "12345"); 			
 			//Leitura (SELECT)
@@ -25,7 +28,6 @@ public class LeituraBancoEscritaTxt {
 			ResultSetMetaData rsmd = rs.getMetaData(); 
 			int numberOfColumns = rsmd.getColumnCount(); 
 			int rowCount = 1; 
-			
 			//Imprimindo os resultados no terminal		
 			System.out.println("Modelo (entrada): \n"); 
 			while (rs.next()) { 
@@ -43,9 +45,18 @@ public class LeituraBancoEscritaTxt {
 		   	stmt.close(); 
 		   	connection.close(); 
 
-       } catch(SQLException ex) { 
-           System.err.print("SQLException: "); 
-           System.err.println(ex.getMessage());} 
+			
+	       } catch(SQLException ex) { 
+		   System.err.print("SQLException: "); 
+		   System.err.println(ex.getMessage());} 
+		
+		try{
+			FileWriter writer = new FileWriter("CodigoGerado.txt");
+			writer.write("Este será o código gerado pela minha dsl!!");
+			writer.close(); 
+		}catch(IOException ex) {
+			ex.printStackTrace();
+		}
 		
 	}
 }
