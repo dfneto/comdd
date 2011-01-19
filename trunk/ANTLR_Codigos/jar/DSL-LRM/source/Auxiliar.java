4d3d3a49719de1a35ce56bb95cff150c60956f55
davidfneto@gmail.com <davidfneto@gmail.com@fb8313af-4c01-193a-e81d-37fb70c46c91>
2011-01-19 19:27:04 +0000

    Principal chama Auxiliar e depois ExecutarDsl

---
+++ trunk/ANTLR_Codigos/jar/DSL-LRM/source/Auxiliar.java
@@ -0,0 +1,75 @@
+//A classe Auxiliar serve para auxiliar a classe TEST (que executa a MyDsl.g) realizando as seguintes atividades:
+//1.Leitura do banco (Modelo),  2.escrita do Modelo num txt. Esse Modelo servirá de entrada para minha dsl.jar (TEST.java) que irá 
+//transformar em códigofonte.cpp (Atividade da Classe TEST)
+//3. Inserir o CódigoFonte.cpp deve ser inserido em uma tabela
+
+import java.sql.*;
+import java.io.*;
+
+public class Auxiliar {
+	//public static void main(String[] argv) {
+		Connection connection = null;
+		String leitura = "select xwd_content from xwikidoc where xwd_name='TesteNome'"; 
+		String ValorLido = null;
+		Statement stmt; 		
+		String url = "jdbc:postgresql://143.107.231.251:5432/xwiki";
+ 		
+	public void estabelecerConexaoComBanco() {
+		//Carregando o Driver do Postgre
+		try {
+ 			Class.forName("org.postgresql.Driver"); 
+		} catch (ClassNotFoundException e) {
+ 			System.out.println("Where is your PostgreSQL JDBC Driver? \n Include in your library path!");
+			e.printStackTrace();
+			return;	}
+		//estabelecendo conexão com o banco		
+		try {	 
+			connection = DriverManager.getConnection(url,"postgres", "12345"); 
+		} catch(SQLException ex) {
+	                System.err.print("SQLException: ");
+                        System.err.println(ex.getMessage());}
+        }
+
+	public void leituraDoBanco() {
+		try {
+			//Leitura (SELECT)
+			stmt = connection.createStatement(); 
+			ResultSet rs = stmt.executeQuery(leitura); 
+			
+			rs.next();
+			ValorLido = rs.getString(1);
+			System.out.println("Modelo extraído da Xwiki:\n\n"+ValorLido);
+
+			//ResultSetMetaData rsmd = rs.getMetaData(); 
+			//int numberOfColumns = rsmd.getColumnCount(); 
+						
+			//Imprimindo os resultados no terminal		
+			//System.out.println("Modelo (entrada): \n"); 
+			/*while (rs.next()) {  //rs.next será true enquanto tiver linhas a percorrer
+				ValorLido = rs.getString(1);
+				for (int i = 1; i <= numberOfColumns; i++) { 
+					System.out.println(rs.getString(i)); 
+					System.out.println(i); 	
+			        } 
+		       	}*/
+			
+		   	stmt.close(); 
+		   	connection.close(); 
+
+			
+		} catch(SQLException ex) { 
+			System.err.print("SQLException: "); 
+		   	System.err.println(ex.getMessage());} 
+	}
+	public void escritaDoModelo() {
+		//Escrever o Modelo lido do banco num arquivo
+		try{
+			FileWriter writer = new FileWriter("/home/david/comdd/ANTLR_Codigos/jar/DSL-LRM/classes/Modelo/Modelo");
+			writer.write(ValorLido);
+			writer.close(); 
+			System.out.println("@@ Foi criado um arquivo com o modelo extraído da Xwiki.................");
+		} catch(IOException ex) {
+			ex.printStackTrace();}
+	}
+	
+}
---
+++ trunk/ANTLR_Codigos/jar/DSL-LRM/source/Principal.java
@@ -0,0 +1,25 @@
+//Esta classe é responsável por chamar a classe auxiliar e a test
+
+//import java.io.*;
+public class Principal {
+	public static void main(String[] args) {
+		
+		System.out.println("@@ Iniciando ....");
+		//Ações de Auxiliar		
+		Auxiliar aux = new Auxiliar();
+		aux.estabelecerConexaoComBanco();
+		aux.leituraDoBanco();
+		aux.escritaDoModelo();
+
+		ExecutarDsl dsl = new ExecutarDsl();
+		try{	
+			System.out.println("@@ Chamando a classe ExecutarDsl..........................\n");
+			System.out.println("======= Código Gerado =========\n");
+			dsl.executeDsl(); 
+			System.out.println("@@ A classe ExecutarDsl foi executada com sucesso .........................\n");
+		} catch (Exception e) {
+			System.out.println("Fodeo");}
+
+	}
+}
+		
