fc74cbf13a1097de0aee884d053e6edc657e1a29
davidfneto@gmail.com <davidfneto@gmail.com@fb8313af-4c01-193a-e81d-37fb70c46c91>
2011-01-20 16:20:44 +0000

    Principal chama o gerador de código (ExecutarDsl) que lê o modelo e gera o arquivo CódigoGerado.cpp na pasta classes. A classe Principal ainda mostra no terminal o andamento de execução do sistema, identificando em que parte está.

---
+++ trunk/ANTLR_Codigos/jar/DSL-LRM/classes/CodigoGerado.cpp
@@ -0,0 +1,7 @@
+Defines Adicionados!!
+Includes Adicionados!!
+GPS criado com sucesso!
+Valor do $tipoSensor = gps
+Compass criado com sucesso!
+Valor do $tipoSensor = bussola
+Robo pionner criado com sucesso!! 
