bfb1c733cb8aefb896e58f7bcea230bb10182bf1
davidfneto <davidfneto@gmail.com>
2011-02-01 13:44:13 +0000

    Esta versao da DSL está rodando perfeitamente num servlet e funcionando na web Abraços!

--- ANTLR_Codigos/DSL na Wiki/DSL/WEB-INF/source/ExecutarDsl.java
+++ ANTLR_Codigos/DSL na Wiki/DSL/WEB-INF/source/ExecutarDsl.java
@@ -7,11 +7,12 @@ public class ExecutarDsl {
 	//public static void main(String[] args) throws Exception {
 	public static void executeDsl() throws Exception {
 		PrintStream padrao = System.out;
-		PrintStream fileLog = new PrintStream(new File("CodigoGerado.cpp")); 
+		PrintStream fileLog = new PrintStream(new File("/home/david/Xwiki/apache-tomcat-6.0.30/webapps/DSL/WEB-INF/classes/Modelo/CodigoGerado.cpp")); 
 		System.setOut(fileLog);//Joga tudo que sairia na saída padrão (no caso o código gerado) num arquivo
 
 		// create a CharStream that reads from standard input
-		ANTLRFileStream input = new ANTLRFileStream("./Modelo/Modelo");
+//		ANTLRFileStream input = new ANTLRFileStream("./Modelo/Modelo"); -> nao funciona :/
+		ANTLRFileStream input = new ANTLRFileStream("/home/david/Xwiki/apache-tomcat-6.0.30/webapps/DSL/WEB-INF/classes/Modelo/Modelo");
 		// create a lexer that feeds off of input CharStream
 		MyDslLexer lexer = new MyDslLexer(input);
 		// create a buffer of tokens pulled from the lexer
@@ -20,7 +21,7 @@ public class ExecutarDsl {
 		MyDslParser parser = new MyDslParser(tokens);
 		// begin parsing at rule r
 		parser.stat();
-				
+					
 		System.setOut(padrao); //Volta ao normal: joga tudo que sairia no log na saída padrao
 		}	
 	
